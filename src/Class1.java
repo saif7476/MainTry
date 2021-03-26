2752bafce3f4381c1f9e372951d2be5604eed759
Saif Karnawi <saifstevens@gmail.com>
2021-03-25 21:06:06 -0700

    Hi

--- src/Class1.java
+++ src/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my class");
-		System.out.println("This is my class2"); 
+		System.out.println("This is my class2");  
 	}
 
 }
